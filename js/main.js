58664a8b73afdb4c85d63b9021e503fb8c7248ed
sdeix <sdeix2765@gmail.com>
2024-01-15 20:35:23 +0700

    задание №5

--- js/main.js
+++ js/main.js
@@ -13,14 +13,33 @@ let app = new Vue({
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
          ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],         
-    }
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],   
+        cart: 0,      
+    },
+    methods: {
+        removeFromCart(){
+            if (this.cart>0) {
+                this.cart-=1
+            } 
+        },
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+         }
+         
+     }
+     
  })
+ 
  
