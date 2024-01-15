c771be3004a22b2e33fc036b19277a0a63fa7449
sdeix <sdeix2765@gmail.com>
2024-01-15 20:19:25 +0700

    задание №3

--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@ let app = new Vue({
         image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inStock: true,
+        inventory: 100,
+        onSale: true,
     }
  })
  
