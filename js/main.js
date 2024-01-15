6ecbb36e0534c7e73c6ddf2279b8204e27dac384
sdeix <sdeix2765@gmail.com>
2024-01-15 21:28:06 +0700

    задание №7

--- js/main.js
+++ js/main.js
@@ -1,45 +1,61 @@
 let app = new Vue({
-    el: '#app',
-    data: {
-        product: "Socks",
-        description: "A pair of warm, fuzzy socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
-        altText: "A pair of socks",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: true,
-        inventory: 100,
-        onSale: true,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green',
-                variantImage: "./assets/vmSocks-green-onWhite.jpg",
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue',
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-            }
-         ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],   
-        cart: 0,      
+  el: "#app",
+  data: {
+    product: "Socks",
+    brand: "Vue Mastery",
+    description: "A pair of warm, fuzzy socks",
+    selectedVariant: 0,
+    altText: "A pair of socks",
+    link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+    inventory: 100,
+    onSale: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: "./assets/vmSocks-green-onWhite.jpg",
+        variantQuantity: 10,
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+        variantQuantity: 0,
+      },
+    ],
+    sizes: ["S", "M", "L", "XL", "XXL", "XXXL"],
+    cart: 0,
+  },
+
+  methods: {
+    removeFromCart() {
+      if (this.cart > 0) {
+        this.cart -= 1;
+      }
     },
-    methods: {
-        removeFromCart(){
-            if (this.cart>0) {
-                this.cart-=1
-            } 
-        },
-        addToCart() {
-            this.cart += 1
-        },
-        updateProduct(variantImage) {
-            this.image = variantImage
-         }
-         
-     }
-     
- })
- 
- 
+    addToCart() {
+      this.cart += 1;
+    },
+    updateProduct(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
+    },
+    sale(){
+        if(this.onSale){
+            return "Проводится распродажа " + this.brand + this.product
+        }
+        return "Распродажа не проводится"
+    }
+  },
+});
